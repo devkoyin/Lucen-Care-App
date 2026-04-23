aa87d8e5f6832ac81714a51637151c45aff11288
Omolara-Aworeni <omolaramary735@gmail.com>
2026-04-23 11:57:24 +0100

    feat: configure root routing and app config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/public/public.routes').then(m => m.PUBLIC_ROUTES),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
+  },
+  {
+    path: 'patient',
+    loadChildren: () =>
+      import('./features/patient/patient.routes').then(m => m.PATIENT_ROUTES),
+  },
+  {
+    path: 'ngo',
+    loadChildren: () =>
+      import('./features/ngo/ngo.routes').then(m => m.NGO_ROUTES),
+  },
+  {
+    path: 'hmo',
+    loadChildren: () =>
+      import('./features/hmo/hmo.routes').then(m => m.HMO_ROUTES),
+  },
+  {
+    path: 'researcher',
+    loadChildren: () =>
+      import('./features/researcher/researcher.routes').then(m => m.RESEARCHER_ROUTES),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
+  },
+  { path: '**', redirectTo: '' },
+];
