a90b32d220ac61e25309e152bfc2db7e3ae1867f
OlutundeJoshua <joshuafavour81@gmail.com>
2026-07-23 19:31:17 +0100

    added forwarded port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://2sbl046f-3000.uks1.devtunnels.ms/api', //replace with your local API URL
   groqApiKey: '',
 };
